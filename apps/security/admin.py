bfff586ee7041152ddff74d355173c0aae71e2e2
Luis Abhram <luizabhram@gmail.com>
2025-12-05 23:09:16 +0800

    Register `Site` model to admin with Unfold styling

--- apps/security/admin.py
+++ apps/security/admin.py
@@ -1,3 +1,21 @@
 from django.contrib import admin
+from django.contrib.sites.models import Site
+from unfold.admin import ModelAdmin
 
-# Register your models here.
+# Unregister the default Site model admin
+admin.site.unregister(Site)
+
+
+# Site Model
+@admin.register(Site)
+class SiteAdmin(ModelAdmin):
+    list_display = (
+        "id",
+        "name",
+        "domain",
+    )
+    search_fields = (
+        "id",
+        "name",
+        "domain",
+    )
